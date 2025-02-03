55d7d3cea89bc99177d71f0ef3814c645f907355
Aaryan1703 <aryanphadtare007@gmail.com>
2025-02-03 17:39:43 +0530

    03-02-24

---
+++ CountOccuranceOfEachWords.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class CountOccuranceOfEachWords {
+    public static void main(String[] args) {
+        String str="Test automation java automation";
+
+        HashMap<String,Integer>countHashMap=new HashMap<>();
+        for (String s:str.split(" ")){
+            if(countHashMap.containsKey(s)){
+                countHashMap.put(s,countHashMap.get(s)+1);
+            }else {
+                countHashMap.put(s,1);
+            }
+        }
+        System.out.println(countHashMap);
+    }
+}
---
+++ PalindromeString.java
@@ -0,0 +1,19 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str="aryan";
+        int start=0;
+        String newStr="";
+        while(start<str.length()){
+            char i=str.charAt(start);
+            newStr=newStr+i;
+            start++;
+        }
+        System.out.println(str);
+        System.out.println(newStr);
+        if (newStr.equals(str)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
